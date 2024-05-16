f3a1b602dcff66aaf938f6564e0292d11199662c
Kushagra1480 <kkartik@asu.edu>
2024-05-16 03:10:08 -0700

    Initialized Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Avatar, Box, Grid, List, ListItem, Stack, Typography, Item, Card, CardMedia, CardContent, CardHeader, CardActionArea, Divider } from "@mui/material";
+import displayPicture from './assets/IMG_6081.jpg'
+import asuLogo from './assets/ASU-logo.webp'
+import seidmanPreview from './assets/Screenshot 2024-05-15 045704.png'
+import nciPreview from './assets/Screenshot 2024-05-15 045850.png'
+import Work from "./Work";
+import Education from "./Education";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Grid container spacing={1} mt={3} >
+        <Grid item xs={2} ml={3}>
+          <Avatar 
+            src={displayPicture} 
+            alt="Kushagra Kartik" 
+            sx={{height: 200, width: 200}}
+            variant="rounded"></Avatar>
+        </Grid>
+        <Grid item xs={8} mt={8} mr={4}>
+          <Typography 
+            variant="h3" 
+            mr={4} 
+            sx={{fontFamily: "Lato", fontWeight: 'bold'}}>
+          Kushagra Kartik</Typography>
+          <Grid container spacing={0}>
+            <Grid item>
+              <Typography variant="h6" mt={4} sx={{fontFamily: "Lato"}}>
+                  Graduate Student at
+              </Typography>
+            </Grid>
+            <Grid item mt={2}>
+              <Avatar 
+                  src={asuLogo} 
+                  variant="rounded"
+                  alt="ASU" 
+                  sx={{width:110, height:60}}
+                  mt={9}>
+              </Avatar>
+            </Grid>
+            <Grid item>
+            <Typography variant="h6" mt={4} sx={{fontFamily: "Lato"}}>
+                  till May'25 
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Work />
+      <Education />
+    </Box>
   );
 }
 
---
+++ src/Education.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Avatar, Box, Grid, List, ListItem, Stack, Typography, Item, Card, CardMedia, CardContent, CardHeader, CardActionArea, Divider } from "@mui/material";
+
+function Education() {
+    return(
+        <Box>
+        <Typography variant="h4" sx={{fontFamily: 'Lato', fontWeight: 'bold'}} mt = {4} ml={2} mb={1}>
+          Work Experience:
+        </Typography>
+        <Card>
+          <CardContent>
+            <Typography>Programming Technician</Typography>
+            <Typography>WP Carey Seidman Research Institute</Typography>
+            <Typography>Jan 2024-Ongoing</Typography>
+            <Typography>Tempe, AZ</Typography>
+            <List>
+              <ListItem>
+                -Manage deployments of https://seidmaninstitute.com/ and https://ncisouthwest.org/
+              </ListItem>
+              <ListItem>
+                -Extract data from the Bureau of Labor Statistics for calculating job growth
+              </ListItem>
+            </List>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent>
+            <Typography>Research Intern</Typography>
+            <Typography>Netaji Subhas Institute of Technology</Typography>
+            <Typography>Feb 2022-March 2022</Typography>
+            <Typography>New Delhi, India</Typography>
+            <List>
+              <ListItem>
+                Worked on dataset creation and cleaning for a sentiment analysis model.
+              </ListItem>
+            </List>
+          </CardContent>
+        </Card>
+      </Box>
+    )
+}
+
+export default Education
